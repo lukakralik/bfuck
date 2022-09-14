19be5e92f86b3b0f1ac9fd182fbd42600f0da823
lukakralik <luka.kralik@gmai.com>
2022-09-14 21:41:23 +0200

    Basic skeleton provided

---
+++ main.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int array[30000], z;
+unsigned int pos;
+
+int main() {
+	pos = 0;
+	while (getchar() != EOF) {
+		switch (getchar()) {
+			case '+':
+				array[pos]++;
+				break;
+			case '-':
+				array[pos]--;
+				break;
+			case '<':
+				pos--;
+				break;
+			case '>':
+				pos++;
+				break;
+		}
+	}
+	return 0;
+}
